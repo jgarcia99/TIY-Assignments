46764ae3872e254beae373f7b8b8beaeb3be2f94
Josuel C Garcia <garciajosuel@yahoo.com>
2015-01-21 18:18:42 -0500

    @jaredstevick starting pair programming

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+var assert = require('assert');
+
+
+/**
+ * @param Number A
+ * @param Number B
+ * @return sum of A and B
+ */
+function add(A , B) {
+    
+    return(A + B);
+}
+
+/**
+ * @param Number A
+ * @param Number B
+ * @return difference of A and B
+ */
+function diff(A, B){
+    
+    return(A-B);
+    
+}
+
+/**
+ * @param Number A
+ * @param Number B
+ * @return product of A times B
+ */
+function prod(A, B){
+    return(A*B);
+}
+
+/**
+ * @param Number A
+ * @param Number B
+ * @return division of A by B
+ */
+function div(A, B){
+    return(A / B);
+}
+
+//console.log(prod(4,5));
+
+assert.equal(diff(4, 3),19)
