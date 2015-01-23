d6ce11ea0324ea797f51f8a501cfbca6233ce316
Josuel C Garcia <garciajosuel@yahoo.com>
2015-01-23 10:27:12 -0500

    @jaredstevick started string calc

---
+++ scripts/string-calculator.js
@@ -0,0 +1,43 @@
+var assert = require('assert');
+
+
+/* function plus(A, B){
+    return (2);
+}
+
+ it('should add "one" and "one"', function(){
+  assert.equal(plus("one", "one"), 2);
+}); 
+
+function plus(A, B){
+    return (0);
+}
+
+it('should add "zero" and "zero"', function(){
+  assert.equal(plus("zero", "zero"), 0);
+}); 
+
+
+function plus(A, B){
+    return (3);
+}
+
+it('should add "one" and "two"', function(){
+  assert.equal(plus("one", "two"), 3);
+}); 
+
+function plus (A,B) {
+    return (4);
+}
+
+it ('should add "one" and "three"', function() {
+        assert.equal(plus("one", "three"), 4);
+    }); */
+
+function plus (A,B) {
+    return (1);
+}
+
+it ('should add "zero" and "one"', function () {
+        assert.equal(plus("zero", "one"), 1);
+});
