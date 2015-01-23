6cbd6627dbdf5e2e0a106524e2d3bc3fbb2cb190
Josuel C Garcia <garciajosuel@yahoo.com>
2015-01-23 14:27:31 -0500

    set up check writing

---
+++ scripts/check-writing.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+function toEnglish(A,B) {
+    
+}
+                     
+
+
+it ('should 
+assert.equal(toEnglish(1), "one");
+assert.equal(toEnglish(2), "two");
+
