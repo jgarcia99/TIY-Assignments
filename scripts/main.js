c8b26b06c3757a4679eb7e50e5158949fbd6b42c
Josuel C Garcia <garciajosuel@yahoo.com>
2015-01-24 16:01:03 -0500

    moved the other side once BUT somehow it was the bishop instead of the pawn

--- scripts/main.js
+++ scripts/main.js
@@ -20,12 +20,14 @@ console.log(board.join('\n') + '\n\n');
 
 
 // Move King's Pawn forward 2
-board[4][4] = board[6][4];
-board[6][4] = ' ';
-console.log(board.join('\n'));
+//board[4][4] = board[6][4];
+//board[6][4] = ' ';
+//console.log(board.join('\n'));
 
 // Move Queens Pawn forward 2
-board[][] = board [][];
+board[5][5] = board [7][5];
+board[7][5] = ' ';
+console.log(board.join('\n'));
 //
 //function print (board) {
 //    
