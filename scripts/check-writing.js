0ab9c84b594c048df2f4857e097f78c7c6736d9b
Josuel C Garcia <garciajosuel@yahoo.com>
2015-01-25 18:56:23 -0500

    check writing done

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -1,12 +1,124 @@
 var assert = require('assert');
 
-function toEnglish(A,B) {
-    
-}
-                     
+//
+//
+//function toEnglish(a) {
+//    if(a == 1) {
+//    return "one";
+//    } else {
+//        return "this is not number 1";
+//    }
+//                     
+//};
+//
+//it ('should equal the english word "one" instead of "1"', function() {
+//assert.equal(toEnglish(1), "one");
+//});
 
+//function toEnglish (a) {
+//    if (a == 2) {
+//        return "two"
+//    } else {
+//        return "this is not number 2";
+//    }
+//};
+//
+//it ('should equal the english word "two" instead of "2"', function (){
+//    assert.equal(toEnglish(2), "two");
+//});
 
-it ('should 
-assert.equal(toEnglish(1), "one");
-assert.equal(toEnglish(2), "two");
+//function toEnglish (a) {
+//    if (a == 3) {
+//        return "three"
+//    } else {
+//        return "this is not number 3";
+//    }
+//}
+//
+//it ('should equal the english word "three" instead of "3"', function () {
+//    assert.equal(toEnglish(3), "three");
+//});
 
+//function toEnglish (a) {
+//    if (a == 4) {
+//        return "four"
+//    }   else {
+//        return "this is not numnber 4";
+//    }
+//}
+//
+//it ('should equal the english word "four" instead of "4"', function() {
+//    assert.equal(toEnglish(4), "four");
+//});
+//
+//function toEnglish (a) {
+//    if (a==5) {
+//        return "five"
+//    } else {
+//        return "this is not number 5";
+//    }
+//}
+//
+//it ('should equal the english word "five" instead of "5"', function() {
+//    assert.equal(toEnglish(5), "five");
+//});
+//
+//function toEnglish (a) {
+//    if (a==6) {
+//        return "six"
+//    } else {
+//        return "this is not number 6";
+//    }
+//}
+//
+//it ('should equal the english word "six" instead of "6"', function (){
+//    assert.equal(toEnglish(6), "six");
+//});
+
+//function toEnglish (a) {
+//    if (a==7) {
+//        return "seven"
+//    } else {
+//        return "this is not number 7";
+//    }
+//}
+//
+//it ('should equal the english word "seven" instead of "seven"', function() {
+//    assert.equal(toEnglish(7), "seven");
+//});
+
+//function toEnglish (a) {
+//    if (a==8) {
+//        return "eight"
+//    } else {
+//        return "this is not  number 8";
+//    }
+//}
+//
+//it ('should equal the english word "eight" instead of "8"', function () {
+//    assert.equal(toEnglish(8), "eight");
+//});
+//
+//function toEnglish (a) {
+//    if (a==9) {
+//        return "nine"
+//    } else {
+//        return "this is not a number 9";
+//    }
+//}
+//
+//it ('should equal the english word "nine" instead of "nine"', function () {
+//    assert.equal(toEnglish(9), "nine");
+//});
+
+function toEnglish (a) {
+    if (a==10) {
+        return "ten"
+    } else {
+        return "this is not number 10";
+    }
+} 
+
+it ('should equal the english word "ten" instead of "ten"', function () {
+    assert.equal(toEnglish(10), "ten");
+});
