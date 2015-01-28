fa837ae5e372664f61af3d31f4cfa34042e4d046
Josuel C Garcia <garciajosuel@yahoo.com>
2015-01-28 04:22:02 -0500

    check-writing?

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -123,34 +123,34 @@ var assert = require('assert');
 //    assert.equal(toEnglish(10), "ten");
 //});
 
-
-function toEnglish(a) {
-    if (a == 1) {
-        return "one";
-    } else if (a== 2) {
-        return "two";
-    } else if (a==3) {
-        return "three";
-    } else if (a==4) {
-        return "four";
-    } else if (a==5) {
-        return "five";
-    } else if (a==6) {
-        return "six";
-    } else if (a==7){
-        return "seven";
-    } else if (a==8){
-        return "eight";
-    } else if (a==9) {
-        return "nine"; 
-    } else if (a==10) {
-        return "ten";
-    }
-<<<<<<< HEAD
-}; 
-=======
-    
-}
+//
+//function toEnglish(a) {
+//    if (a == 1) {
+//        return "one";
+//    } else if (a== 2) {
+//        return "two";
+//    } else if (a==3) {
+//        return "three";
+//    } else if (a==4) {
+//        return "four";
+//    } else if (a==5) {
+//        return "five";
+//    } else if (a==6) {
+//        return "six";
+//    } else if (a==7){
+//        return "seven";
+//    } else if (a==8){
+//        return "eight";
+//    } else if (a==9) {
+//        return "nine"; 
+//    } else if (a==10) {
+//        return "ten";
+//    }
+//<<<<<<< HEAD
+//}; 
+//=======
+//    
+//}
 
 var toEnglish=["zero","one","two","three","four","five","six","seven","eight","nine","ten"]  
 
