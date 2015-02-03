4c9871660c4de154caa65e55a047bd89781b0706
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-02 23:45:07 -0500

    MFg WEBSITE BABY

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,24 @@
+function showMe(id) {
+				this.nextElementSibling.className = "open";
+			}
 
+			var h2s = document.getElementsByTagName('h2');
+			for (var index = 0;index < h2s.length; index++) {
+				h2s[index].onclick = showMe;
+			}
+
+ if (this.nextElementSibling.className != "open") {
+                this.nextElementSibling.className = "open";
+            }
+            else {
+                this.nextElementSibling.className = "closed";
+            }
+
+function showMe(){
+            if (this.nextElementSibling.className != "open") {
+                this.nextElementSibling.className = "open";
+            }
+            else {
+                this.nextElementSibling.className = "closed";
+                alert('dont click me again, dick...');
+            }
