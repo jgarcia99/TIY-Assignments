228cf11a40dce4d13d04f9c90b345a17df793e4e
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-09 15:39:06 -0500

    last push

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
+    
+    var userData = $.getJSON("https://api.github.com/users/jgarcia99");
+    
+    userData.done(function(json) {
+        var user = json;
+    });
+    
+});
 
-   $.getJSON('..https://api.github.com/users/jgarcia99', function(json.login) {
-});  
 
-    });  
+
+
+
+
+
+
+
+//$(document).ready(function(){
+//
+//   $.getJSON('..https://api.github.com/users/jgarcia99', function(json.login) {
+//});  
+//
+//      $("#submission").click(function() {
+//       var comment = $("#comments").val();
+//       $("#comments").val("")
+//       $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/261/comments?access_token=e8", 
+//           JSON.stringify({"body": comment})
+//             );
+//       $("#comment-form").submit();
+//   })
+//    });  
