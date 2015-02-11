53b07247f5f9d1c643d2bf4d2b32b0f7fb97dffc
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-11 07:21:11 -0500

    a somewhat starting point

---
+++ pub/js/scripts.js
@@ -0,0 +1,28 @@
+var assert = require('asssert');
+
+// this is the constructor 
+function Place(color,name,position() {
+    
+    this.color = color;
+    this.name = name;
+    this.position = position;
+});
+
+
+piece.prototype = {
+    position: function() {
+        
+        return this.position;
+    },
+    
+    color.prototype = {
+        color: function() {
+        return this.color;
+}
+},
+    name.prototype = {
+        name: function () {
+            return this.name;ß
+        }
+    }
+}
