7e0f63996fe21fd9c0abc14eb3290e80b87bc2b7
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-14 17:22:51 -0500

    tried string calc for 3 hours

---
+++ katas/string-calculator.js
@@ -0,0 +1,98 @@
+var assert = require('assert');
+//
+//function plus (A,B) {
+//   if("one" == A){
+//       return 2;
+//    }       else if("two" == A) {
+//        return 3;
+//    }       else if ("three" == A) {
+//        return 4;
+//    }       else if ("four" == A) {
+//        return 5;
+//    }
+//    }
+
+
+
+
+
+
+//it('should add "two" and "one"', function() {
+//    assert.equal(plus("two", "one"), 3);
+//});
+
+var numbers = {
+    'one': 1,
+	'two': 2,
+	'three': 3,
+	'four': 4,
+	'five': 5,
+	'six': 6,
+	'seven': 7,
+	'eight': 8,
+	'nine': 9,
+	'ten': 10,
+	'eleven': 11,
+	'twelve': 12,
+	'thirteen': 13,
+	'fourteen': 14,
+	'fifteen': 15,
+	'sixteen': 16,
+	'seventeen': 17,
+	'eighteen': 18,
+	'nineteen': 19,
+	'twenty': 20,
+	'thirty': 30,
+	'fourty': 40,
+	'fifty': 50,
+	'sixty': 60,
+	'seventy': 70,
+	'eighty': 80,
+	'ninety': 90,
+	'hundred': 100
+};
+
+function plus(A + B) {
+    
+}
+
+
+var numbers.B = {
+    'one': 1,
+	'two': 2,
+	'three': 3,
+	'four': 4,
+	'five': 5,
+	'six': 6,
+	'seven': 7,
+	'eight': 8,
+	'nine': 9,
+	'ten': 10,
+	'eleven': 11,
+	'twelve': 12,
+	'thirteen': 13,
+	'fourteen': 14,
+	'fifteen': 15,
+	'sixteen': 16,
+	'seventeen': 17,
+	'eighteen': 18,
+	'nineteen': 19,
+	'twenty': 20,
+	'thirty': 30,
+	'fourty': 40,
+	'fifty': 50,
+	'sixty': 60,
+	'seventy': 70,
+	'eighty': 80,
+	'ninety': 90,
+	'hundred': 100
+};
+function plus (A,B) {
+    return numbers.'one'[a] + numbers[b];
+    return ("t);
+
+};
+
+it('should add "one" and "one"', function() {
+    assert.equal(plus("one", "one"), 2);
+});
