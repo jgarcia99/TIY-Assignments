41fa648e2ce105fd1118aaa004f9f0aca64385bd
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-15 14:48:34 -0500

    got pushevents with _.result

--- apis/gymnastics.js
+++ apis/gymnastics.js
@@ -1,6 +1,11 @@
 var events = require('../apis/github/users/jgarcia99/events.json');
 var _ = require('lodash');
 
-var totalEvents = _.pluck(events,"type" )
+var totalEvents = _.pluck(events,"type");
 
-console.log(totalEvents.length);
+var pushEvents = _.result(events, "type", "PushEvent");
+
+
+
+//console.log(totalEvents.length);
+console.log(pushEvents.length);
