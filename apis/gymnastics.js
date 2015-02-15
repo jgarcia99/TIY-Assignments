3840b96701f08096cafae1599c1327a986e45b3b
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-15 17:15:05 -0500

    ok moving on to github profile

--- apis/gymnastics.js
+++ apis/gymnastics.js
@@ -5,7 +5,9 @@ var totalEvents = _.pluck(events,"type");
 
 var pushEvents = _.result(events, "type", "PushEvent");
 
-
+var pushPerDay = _.result(pushEvents, "created_at", function() {
+    if ("created_at" <= )
+})
 
 //console.log(totalEvents.length);
 console.log(pushEvents.length);
